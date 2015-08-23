e66af3f273c6459b5e511c05d39ca1510daa47ae
chestofwonder <miguelarroyo@MacBook-de-Miguel.local>
2015-08-23 19:34:46 +0200

    Gallery

--- web/1 - DEVELOPMENT/js/slider.js
+++ web/1 - DEVELOPMENT/js/slider.js
@@ -1,51 +1,36 @@
+
 // JavaScript Document
 
 jQuery(document).ready(function(){
-	preloader();	
-});
 
-
-function preloader(){
-	var slides = jQuery('.slideShow > li');
-	var slideCount = 0;
-	var totalSlides = slides.lenght;
-	var slideCache = [];
+	//Automatic slider
+	var width = 200;
+	var speed = 2000;
+	var pause = 4000;
 	
-	if(slideCount < totalSlides){
-		slideCache[slideCount] = new Image();
-		slideCache[slideCount].src = slides.eq(slideCount).find('img').attr('src');
-		slideCache[slideCount].onload = function(){
-			slideCount++;
-			preloader();
-		}
-	}else{
-		slideCount = 0;
-		SlideShow();
+	var slider = jQuery('.slides');
+	var slides = jQuery('.slides li');
+	var currentSlide = 1;
+	
+	var interval;
+	playSlider();
+	
+	function playSlider(){
+		interval = setInterval(function(){
+			slider.animate({marginLeft:'-=' + width}, speed, function() {
+				currentSlide++;
+				if (currentSlide == slides.length){
+					currentSlide = 1;
+					slider.css('margin-left', 0);
+				}
+			}); 
+		}, pause);
 	}
-}
-/*
-function SlideShow(){
-	//animate{
-		callback:
-			slideCount < totalSlides - 1? slideCount++ : slideCount = 0;
-			SlideShow()
+	
+	function stopSlider(){
+		clearInterval(interval);
 	}
-}
-
-
-
-$( "#clickme" ).click(function() {
-  $( "#book" ).animate({
-    opacity: 0.25,
-    left: "+=50",
-    height: "toggle"
-  }, 5000, function() {
-    // Animation complete.
-  });
+	
 });
 
 
-
-
-
-*/
